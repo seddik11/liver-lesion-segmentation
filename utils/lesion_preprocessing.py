a78f03410015a516e5b6ac5a650eca54c4a9c92c
seddik11 <seddiks@outlook.fr>
2019-09-11 16:15:41 +0100

    Update lesion_preprocessing.py

--- utils/lesion_preprocessing.py
+++ utils/lesion_preprocessing.py
@@ -6,7 +6,7 @@ import dpp.threedim.segmentation.medical as vol
 input_identifier = "volume"
 ground_truth_identifier = "segmentation"
 
-train_split = [.7, 0.3]
+train_split = [.3, 0.7]
 val_split = [0.85, 0.15]
 seed = 42
 
